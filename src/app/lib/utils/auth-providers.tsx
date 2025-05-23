9a5ed7b5262804f7d1d48ad5baf4d20f9063554f
mat_dweb <mat.dweb28@gmail.com>
2025-05-23 14:23:53 -0600

    feat(utils): add signInWithProvider()

---
+++ src/app/lib/utils/auth-providers.tsx
@@ -0,0 +1,29 @@
+import { GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
+import { auth } from "@/app/lib/firebase/firebase";
+import { signInWithPopup } from 'firebase/auth';
+import { extractErrorDetails } from './errrors';
+import { setAuthCookie } from './auth';
+import { errorResponse, successResponse } from './response';
+import type { ThirdPartyProvidersNames } from '@/components/Form/AuthForm';
+
+export const signInWithProvider = async (providerName: ThirdPartyProvidersNames) => {
+    const provider =
+        providerName == "google" ? (new GoogleAuthProvider())
+            : ((providerName == "github") && new GithubAuthProvider()) as GithubAuthProvider;
+
+    provider.setCustomParameters({ prompt: "select_account" });
+    try {
+        const result = await signInWithPopup(auth, provider);
+        const token = await result.user.getIdToken();
+
+        await setAuthCookie(token);
+
+        return successResponse([(`Logged in with ${providerName}`)]);
+
+    } catch (error) {
+        const { message = "Unexpected error occurred." } = extractErrorDetails(error);
+        return errorResponse([`Failed to Login with ${providerName}`], {
+            providers: [message]
+        })
+    }
+}
