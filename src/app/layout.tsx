26298d6a92d580e716de4cd7bebbafe8d165b1b5
mat_dweb <mat.dweb28@gmail.com>
2025-05-28 22:27:02 -0600

    feat: add AuthBilders wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 import { geistSans, geistMono, ibmPlexSans } from '@/app/ui/fonts'
-import { CountDownTimer } from "@/components/CountDownTimer";
-import { verifySession } from "./lib/dal";
-import { UserInfo } from "@/components/UserInfo";
-import NavHeader from "@/components/NavHeader";
-import SessionErrorToastHandler from "@/components/Handlers/SessionErrorToastHandler";
+import { CountDownTimer } from "@/components/(AuthBilders)/CountDownTimer";
+import { verifySession } from "./lib/(AuthBilders)/dal";
+import { UserInfo } from "@/components/(AuthBilders)/UserInfo";
+import NavHeader from "@/components/(AuthBilders)/NavHeader";
+import SessionErrorToastHandler from "@/components/(AuthBilders)/Handlers/SessionErrorToastHandler";
+import { AuthBilders } from "./AuthBilders";
 
 export const metadata: Metadata = {
   title: "AuthBilders | Firabse",
@@ -25,15 +26,12 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${ibmPlexSans.variable} antialiased`}
       >
-        <Providers>
+        <AuthBilders>
           <section className="w-full flex min-h-screen flex-col justify-start bg-gradient-to-r from-[#12222b] to-[#0e0e0e]">
             <NavHeader />
             {children}
-            {<SessionErrorToastHandler decoded={decoded} />}
-            {<CountDownTimer data={decoded} />}
-            {<UserInfo data={decoded} />}
           </section>
-        </Providers>
+        </AuthBilders>
       </body>
     </html >
   );
