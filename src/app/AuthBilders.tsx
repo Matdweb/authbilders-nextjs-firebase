d33304875a10a2860098d213c56a03a7a53186bb
mat_dweb <mat.dweb28@gmail.com>
2025-05-30 14:41:11 -0600

    feat :Add AuthBilders wrapper

---
+++ src/app/AuthBilders.tsx
@@ -0,0 +1,18 @@
+import { Providers } from "./providers";
+import { CountDownTimer } from "@/components/(AuthBilders)/CountDownTimer";
+import { verifySession } from "./lib/dal";
+import { UserInfo } from "@/components/(AuthBilders)/UserInfo";
+import SessionErrorToastHandler from "@/components/(AuthBilders)/Handlers/SessionErrorToastHandler";
+
+export default async function AuthBilders({ children }: { children: React.ReactNode; }) {
+    const decoded = await verifySession();
+
+    return (
+        <Providers>
+            {children}
+            {<SessionErrorToastHandler decoded={decoded} />}
+            {<CountDownTimer data={decoded} />}
+            {<UserInfo data={decoded} />}
+        </Providers>
+    );
+}
